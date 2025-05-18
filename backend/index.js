e94fd97fc8055a0f1504b09f4fa5042bd839e107
Edwin Zeledon <edwzeledon80@gmail.com>
2025-05-18 14:18:45 -0700

    chore: add dotenv and node-fetch dependencies

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import pg from 'pg';
-import cors from 'cors'
+import cors from 'cors';
+import fetch from 'node-fetch';
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const port = 3000
 const app = express()

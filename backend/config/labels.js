050860c411c7411694437e5f82fb9e3211963327
Edwin Zeledon <edwzeledon80@gmail.com>
2025-06-05 17:07:31 -0700

    refactor: move HF and news services to new folders

---
+++ backend/config/labels.js
@@ -0,0 +1,22 @@
+const LABELS = [
+    'product update',
+    'new product launch',
+    'technical issue',
+    'cybersecurity breach',
+    'leadership change',
+    'layoffs',
+    'hiring',
+    'earnings report',
+    'merger or acquisition',
+    'funding announcement',
+    'regulatory approval',
+    'legal issue',
+    'scandal',
+    'customer backlash',
+    'philanthropy',
+    'market expansion',
+    'pricing change',
+    'not business relevant'
+]
+
+export default LABELS
---
+++ backend/services/hf/hfService.js
@@ -0,0 +1,67 @@
+import dotenv from 'dotenv';
+dotenv.config({ path: '../../.env'})
+import LABELS from '../../config/labels.js'
+
+const hfurl = 'https://api-inference.huggingface.co/models'
+
+export const classifyHeadline = async (text) => {
+    const response = await fetch(`${hfurl}/facebook/bart-large-mnli`,
+        {
+            method: "POST",
+            headers: {
+                Authorization: `Bearer ${process.env.HF_TOKEN}`,
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                inputs: text,
+                parameters: { candidate_labels: LABELS }
+            }),
+        }
+    )
+    if (response.ok){
+        const results = await response.json()
+        return results.labels[0]
+    }
+    return 'undefined'
+}
+
+export const getSentiment = async (text) => {
+    const response = await fetch(`${hfurl}/cardiffnlp/twitter-roberta-base-sentiment`, {
+        method: 'POST',
+        headers: {
+            Authorization: `Bearer ${process.env.HF_TOKEN}`,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ inputs: text })
+    })
+    return await response.json()
+    /* 
+       output: 
+       [
+          [
+               { label: 'LABEL_2', score: 0.9931817650794983 }, // Positive
+               { label: 'LABEL_1', score: 0.004856493324041367 }, // Nuetral
+               { label: 'LABEL_0', score: 0.0019616682548075914 }' // Negative
+           ]
+       ]
+   */
+}
+
+export const summarize = async(text) => {
+    const response = await fetch(`${hfurl}/facebook/bart-large-cnn`, {
+        method: 'POST',
+        headers: {
+            Authorization: `Bearer ${process.env.HF_TOKEN}`,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            inputs: text,
+            parameters: {
+                min_length: 20,
+                max_length: 100
+            }
+        })
+    })
+    const results = await response.json()
+    return results[0].summary_text
+}
